a1f9934852c546ad50b6e5af2e71bd8202703fbf
sbx_user1051 <sbx_user1051@169.254.11.45>
2024-04-02 14:46:13 +0000

    Tue Apr 02 14:46:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,35 @@
 from collections import Counter
 
+items = {
+    'A': {'price': 50},
+    'B': {'price': 30},
+    'C': {'price': 20},
+    'D': {'price': 15},
+    'E': {'price': 40},
+    'F': {'price': 10},
+    'G': {'price': 20},
+    'H': {'price': 10},
+    'I': {'price': 35},
+    'J': {'price': 60},
+    'K': {'price': 80},
+    'L': {'price': 90},
+    'M': {'price': 15},
+    'N': {'price': 40},
+    'O': {'price': 10},
+    'P': {'price': 50},
+    'Q': {'price': 30},
+    'R': {'price': 50},
+    'S': {'price': 30},
+    'T': {'price': 20},
+    'U': {'price': 50},
+    'V': {'price': 50},
+    'W': {'price': 20},
+    'X': {'price': 90},
+    'Y': {'price': 10},
+    'Z': {'price': 50}
+}
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -30,19 +60,10 @@ def checkout(skus):
             remaining_skus.extend([sku for _ in range(count)])
 
     for sku in remaining_skus:
-        if sku == 'A':
-            total += 50
-        elif sku == 'B':
-            total += 30
-        elif sku == 'C':
-            total += 20
-        elif sku == 'D':
-            total += 15
-        elif sku == 'E':
-            total += 40
-        elif sku == 'F':
-            total += 10
+        if sku in items.keys():
+            total += items[sku]['price']
         else:
             return -1
         
     return total
+
