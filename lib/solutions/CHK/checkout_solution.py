bddc195c6fe6945c0077ab546b1e4196a4bcf0cb
sbx_user1051 <sbx_user1051@169.254.11.45>
2024-04-02 12:28:06 +0000

    Tue Apr 02 12:28:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,38 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+
+    counts = Counter(skus)
+    remaining_skus = []
+
+    for sku, count in counts.items():
+        if sku == 'A':
+            total += (count // 3) * 130
+
+            for _ in range(count % 3):
+                remaining_skus.append(sku)
+
+    print(remaining_skus)
+    exit()
+
+
+
+    for sku in skus:
+        if sku == 'A':
+            total += 50
+        elif sku == 'B':
+            total += 30
+        elif sku == 'C':
+            total += 20
+        elif sku == 'D':
+            total += 15
+        else:
+            return -1
+        
+    return total
+
+if __name__ == "__main__":
+    checkout("AABBAABCD")
